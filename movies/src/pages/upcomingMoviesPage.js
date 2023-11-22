3c89ae20c36e040abc36121deb6f93b152d0b782
Ciaran <ciaran.hickey99@gmail.com>
2023-11-22 22:08:55 +0000

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -3,7 +3,7 @@ import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
-import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 
 const UpcomingMoviesPage = () => {
   const { data, error, isLoading, isError } = useQuery(
@@ -21,17 +21,12 @@ const UpcomingMoviesPage = () => {
 
   const upcomingMovies = data.results;
 
-  // Redundant, but necessary to avoid app crashing.
-  const favorites = upcomingMovies.filter((m) => m.favorite);
-  localStorage.setItem("favorites", JSON.stringify(favorites));
-  const addToFavorites = (movieId) => true;
-
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={upcomingMovies}
       action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />;
+        return <PlaylistAddIcon movie={movie} />;
       }}
     />
   );
